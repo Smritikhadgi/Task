4e48c18c057a676139cf228662f76d0892e3c2a4
Smritikhadgi <smrtikhadgi98@gmail.com>
2022-05-21 12:25:32 +0545

    First commit

---
+++ fridayhw.py
@@ -0,0 +1,100 @@
+# def login(username,email,address,number,password):
+#     print(f"Your username is: {username}.Your email is: {email}.Your address is {address}. Your number is:{ number} and your password is: {password}")
+# login("Sunil","sunil2","Putalisadak","9856871","Battle")
+
+# def operation(x,y,z):
+#     if x>y and x>z:
+#         print(x)
+#     elif y>x and y>z:
+#         print(y)
+#     else:
+#         print(z)
+# operation(30,20,10)
+    
+
+# a=[1,2,3,4]
+# sum=0
+# for i in a:
+#     sum=sum+i
+# print(sum)
+
+# def add():
+#     a=[1,2,3,4]
+# sum=0
+# for i in a:
+#     sum=sum+i
+# print(sum)
+# add()
+
+# a=[1,2,3,4]
+# mul=1
+# for i in a:
+#     mul=mul*i
+# print(mul)
+
+# def product():
+#     b=[1,2,3,4]
+# mul=1
+# for i in b:
+#     pro=mul*i
+# print(pro)
+# product()
+
+# a="Python"
+# for i in range(5,-1,-1):
+#     print(a[i],end="")
+
+# def reverse():
+#     a="Python"
+# for i in range(5,-1,-1):
+#     print(a[i],end="")
+# reverse()
+
+# a=[8,9,10,11]
+# b=[]
+# for i in reversed(a):
+#     b.append(i)
+# print(b)
+
+# def reverse():
+#     a=[8,9,10,11]
+#     b=[]
+# for i in reversed(a):
+#      b.append(i)
+# print(b)
+# reverse()
+
+# def show(name,age):
+#     print(f"Name:{name} Age:{age}")
+# show(name="Sunil",age=20)
+
+# def show(name,age=20):
+#     print(name,age)
+# show(name="sunil")
+
+# def show(*num):
+#     z=num[0]+num[1]+num[2]
+#     print(z)
+# show(5,4,3)
+
+# def show(**num):
+#     z=num['a']+num['b']+num['c']
+#     print(z)
+# show(a=5,b=4,c=3)
+
+# def mul():
+#      a=[8,2,3,-1,7]
+#      b=1
+# for i in range(0,5):
+#      b=a[i]*b
+# print(b)
+# mul()
+
+
+def mul():
+    a=[8,2,3,-1,7]
+    b=1
+    for i in range(0,5):
+        b=a[i]*b
+    print(b)
+mul()
